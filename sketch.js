94de8c7f3b6157aa32a2bac1b8a3b900138ed1f5
nunotshy <114681492+nunotshy@users.noreply.github.com>
2023-02-22 16:42:29 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,24 @@
-var canvas;
-let img1;
-let graphic;
-function setup() {
-  
-  canvas = createCanvas(AUTO, AUTO);
-  canvas.position(0,0);
-  canvas.style('z-index', '-0');
-img1 = loadImage("pano.avif");
-graphic = createGraphics(width, height)
+let sketch1 = function (p) {
+let canvas;
+  let img1;
+  let graphic;
 
-}
+  p.setup = function () {
 
-function draw() {
-  
-  background(img1);
-  
-}
+    p.createCanvas(p.windowWidth, p.windowHeight/2);
 
+   // canvas.p.position(p.windowWidth / 2 - p.width / 2, 60);
+    //canvas.p.style('z-index', '-0');
+    img1 = p.loadImage("sta.jpg");
+    graphic = p.createGraphics(p.width, p.height);
+
+  }
+
+  p.draw = function () {
+
+    p.image(img1, p.width / 2 - img1.width / 4, 60, img1.width/2, img1.height/2 );
+
+  };
+
+};
+var myp5 = new p5(sketch1, 'sk1')
